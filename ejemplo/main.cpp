cb7ad8cd06bfeaf04d4bfe888e2f60f361d2adaa
lemuel <lemuelhelonmarquezrosas@gmail.com>
2023-05-01 23:36:19 -0600

    plantilla

---
+++ ejemplo/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define endls "\n\n\n"
+#define fi first
+#define se second
+#define fast                                                                   \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+
+using ll = long long;
+
+const int maxn = INT_MAX;
+
+int main() {
+  fast;
+  // int t = 1;
+
+  // while (t--) {
+  //   cout << solve() << endl:
+  // }
+  return 0;
+}
